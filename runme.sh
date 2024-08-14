9cee9b97a9027c925f5f3a8c7b931c12925b1c06
Diego <diego.feito@storicard.com>
2024-08-14 11:28:14 -0300

    daf

--- runme.sh
+++ runme.sh
@@ -5,14 +5,9 @@ set -x
 # docker volume create grafana-data
 
 #Stop
-# docker compose --file /home/daf/scripts/docker/prometheus/prometheus/prometheus.yaml --project-name prometheus down
+# docker compose --file ~/scripts/docker/prometheus/prometheus/prometheus.yaml --project-name prometheus down
 sleep 1
 #Start
-docker compose --file /home/daf/scripts/docker/prometheus/prometheus/prometheus.yaml --project-name prometheus up --detach --remove-orphans ## --force-recreate
+docker compose --file ~/scripts/docker/prometheus/prometheus/prometheus.yaml --project-name prometheus up --detach --remove-orphans ## --force-recreate
 sleep 5
 docker restart prometheus grafana 
-
-curl --silent  http://beta.tigris-trout.ts.net:9188/metrics  \
-| grep -i database 
-
-https://beta.tigris-trout.ts.net:9090/graph?g0.expr=pg_database_size_bytes&g0.tab=0&g0.display_mode=lines&g0.show_exemplars=0&g0.range_input=1h
