666c858f3872967218eee475d41836e3bc1b7724
Diego <diego.feito@storicard.com>
2024-08-14 15:38:17 -0300

    arreglo mail de grafana

--- runme.sh
+++ runme.sh
@@ -4,6 +4,10 @@ set -x
 # docker volume create prometheus-data
 # docker volume create grafana-data
 
+# cat ./secrets/smtp_user.conn | docker secret create smtp_user -
+# cat ./secrets/smtp_password.conn | docker secret create smtp_password -
+
+
 #Stop
 # docker compose --file ~/scripts/docker/prometheus/prometheus/prometheus.yaml --project-name prometheus down
 sleep 1
